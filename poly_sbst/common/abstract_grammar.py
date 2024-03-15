26ce0a03a7d079253c8d09d00b65fd974dd39194
andre07cloud <digitexandre@gmail.com>
2024-03-15 00:08:15 -0400

    updating Abstract Grammar code

--- poly_sbst/common/abstract_grammar.py
+++ poly_sbst/common/abstract_grammar.py
@@ -3,10 +3,14 @@ import re
 
 
 
+import random
+import re
+
+
 class AbstractGrammar:
     """
-    # The `AbstractGrammar` class is used to generate strings based on a given grammar.
-    Code partially taken from https://www.fuzzingbook.org/html/Grammars.html#"""
+    A class used to generate strings based on a given grammar.
+    """
 
     def __init__(self, gram: dict):
         self.START_SYMBOL = "<start>"
@@ -41,27 +45,38 @@ class AbstractGrammar:
         term = start_symbol
         expansion_trials = 0
         grammar = self.gram
+        flag = True
 
-        while len(self.nonterminals(term)) > 0:
-            symbol_to_expand = random.choice(self.nonterminals(term))
-            expansions = grammar[symbol_to_expand]
-            expansion = random.choice(expansions)
-            # In later chapters, we allow expansions to be tuples,
-            # with the expansion being the first element
-            if isinstance(expansion, tuple):
-                expansion = expansion[0]
+        while flag:
+            nonterminals = [
+                item for item in self.nonterminals(term) if item in grammar.keys()
+            ]
 
-            new_term = term.replace(symbol_to_expand, expansion, 1)
+            if not nonterminals:
+                break
 
-            if len(self.nonterminals(new_term)) < max_nonterminals:
-                term = new_term
-                if log:
-                    print("%-40s" % (symbol_to_expand + " -> " + expansion), term)
-                expansion_trials = 0
-            else:
-                expansion_trials += 1
-                if expansion_trials >= max_expansion_trials:
-                    print("Cannot expand " + repr(term))
+            for symbol_to_expand in nonterminals:
+                if symbol_to_expand in grammar:
+                    expansions = grammar[symbol_to_expand]
+                    expansion = random.choice(expansions)
+                    # In later chapters, we allow expansions to be tuples,
+                    # with the expansion being the first element
+                    if isinstance(expansion, tuple):
+                        expansion = expansion[0]
+
+                    new_term = term.replace(symbol_to_expand, expansion, 1)
+                    term = new_term
+                    if len(self.nonterminals(new_term)) < max_nonterminals:
+                        if log:
+                            print(
+                                "%-40s" % (symbol_to_expand + " -> " + expansion), term
+                            )
+                        expansion_trials = 0
+                    else:
+                        expansion_trials += 1
+                        if expansion_trials >= max_expansion_trials:
+                            flag = False
+                            break
 
         return term
 
